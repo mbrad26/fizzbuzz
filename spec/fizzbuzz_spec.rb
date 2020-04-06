a564161bab23ff2829fa5e639da159b7b3a13604
pauladarias <pauladarias@gmail.com>
2020-04-06 12:36:10 +0100

    test added for fizbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
 	it 'return "buzz" when passed 5' do 
 		expect(fizzbuzz(5)).to eq 'buzz'
 	end 
+	it 'return "fizzbuzz" when passed 15' do 
+		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+	end 
 end 
