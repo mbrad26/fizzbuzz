51c2dec08fa193436cb3b44684c03aa6ecb06909
mbrad26 <mbrad26@gmail.com>
2020-04-06 11:44:39 +0000

    Added test for any other number

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,13 @@
-
 def fizzbuzz(number)
-  if number % 3 == 0
+  
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
     'fizz'
   elsif number % 5 == 0
-	'buzz'
+	  'buzz'
+	else
+	  number
   end
+  
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-	it 'returns "fizz" when passed 3' do 
+	it 'return "fizz" when passed 3' do 
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end 
 	it 'return "buzz" when passed 5' do 
@@ -10,4 +10,7 @@ describe 'fizzbuzz' do
 	it 'return "fizzbuzz" when passed 15' do 
 		expect(fizzbuzz(15)).to eq 'fizzbuzz'
 	end 
+	it 'return number when passed any other number' do
+	  expect(fizzbuzz(17)).to eq 17
+	end
 end 
